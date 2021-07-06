33454008febd25d8b3af6b1567ad4a998f6bf1d6
andr3qa <andr3qaa@gmail.com>
2021-07-06 17:07:10 +0300

    added video.js library, started making video section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,11 @@
 import './components/burger';
 import './components/scroll';
 import './components/testWebP';
+import videojs from 'video.js';
+// import './../../node_modules/video.js/dist/video.min.js';
+// import './../../node_modules/video.js/dist/lang/ru.js';
+// require('./../../node_modules/video.js/dist/lang/ru.js');
+// require('./../../node_modules/video.js/dist/video.min.js');
+
+videojs(document.querySelector('.video-js'));
 
